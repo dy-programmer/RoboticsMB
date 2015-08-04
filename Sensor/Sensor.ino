118c03fc11980c720ad3b616170e73931c3a12a7
dy-programmer <dly22@hotmail.com>
2015-08-03 20:13:45 -0700

    Avoids obstacles

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -14,7 +14,18 @@ const int sensePin[3]={5,4,3};  // array containing the pin numbers
 
 const int senseOnOffPin=1;
 
-int sensorInput[3]={0}; // sensor inputs
+const int upKey=117;
+const int downKey=118;
+const int leftKey=53;
+const int rightKey=52;
+const int power=22;
+const int forwardSpeedL=160;
+const int forwardSpeedR=110;
+const int reverseSpeedL=-160;
+const int reverseSpeedR=-110;
+const int t=50;
+
+int sensorInput[3]={0};// sensor inputs
 
 void setup()
 {
@@ -22,37 +33,163 @@ void setup()
   pinMode(senseOnOffPin,OUTPUT); // set up sensor on/off pin
   Serial.begin(19200); // set the serial port speed.
   digitalWrite(senseOnOffPin, HIGH);  // Turn on the sensor
+  microM.ircommand=0;
+}
+
+void sensorData(int index)
+{
+  if (index == 0)
+  {
+    sensorInput[index]=analogRead(sensePin[index])-420;//350
+    Serial.print(sensorInput[index]);
+    Serial.print(" "); 
+  } 
+  
+   
+  if (index == 1)
+  {
+    sensorInput[index]=analogRead(sensePin[index])-450;//270
+    Serial.print(sensorInput[index]);
+    Serial.print(" "); 
+  } 
+  
+  if (index == 2)
+  {
+    sensorInput[index]=analogRead(sensePin[index])-560;//360
+    Serial.print(sensorInput[index]);
+    Serial.print(" "); 
+  } 
+}
+
+void remoteControl (int ir_signal)
+{
+if (ir_signal == upKey)
+   {
+     microM.Motors(forwardSpeedL,forwardSpeedR,0,0);
+     microM.ircommand = 0;
+     delay (t);
+   }
+
+else if (ir_signal == downKey)
+   {
+     microM.Motors(reverseSpeedL,reverseSpeedR,0,0);
+     microM.ircommand = 0;
+     delay (t);
+   }
+   
+else if (ir_signal == rightKey)
+   {
+     microM.Motors(forwardSpeedL,0,0,0);
+     microM.ircommand = 0;
+     delay (t);
+   }
+   
+else if (ir_signal == leftKey)
+   {
+     microM.Motors(0,forwardSpeedR,0,0);
+     microM.ircommand = 0;
+     delay (t);
+   }
+
+else if   (ir_signal == power)
+   {
+     microM.Motors(0,0,0,0);
+     microM.ircommand = 0;
+     delay (t);
+   }
+   
+else
+   {
+     microM.Motors(0,0,0,0);
+     microM.ircommand = 0;
+   }
+}
+
+void detectObject (int sensorInfo)
+{   
+ if ((sensorInfo == 1) && (sensorInput[sensorInfo]>220))
+{
+  Serial.print("detectStraight");
+  Serial.print(" ");
+  microM.Motors(0,0,0,0);
+  delay(50); // 50 is just so the code will work. I don't know the actual time length the robot should back up.
+  do
+  {
+    microM.Motors(reverseSpeedL,reverseSpeedR,0,0);
+    delay(50);
+    
+  } while ((sensorInput[0]>300) && (sensorInput[2]>100));
+  if ((sensorInput[0]<300) || (sensorInput[2]>100))
+  {
+    microM.Motors(0,0,0,0);
+    delay (50);
+  }
+  if ((sensorInput[0]<300))
+  {
+    Serial.print("Open Right");
+    Serial.print(" ");
+    microM.Motors(forwardSpeedL,0,0,0);
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(forwardSpeedL,forwardSpeedR,0,0);
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(0,forwardSpeedR,0,0);
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(forwardSpeedL,forwardSpeedR,0,0);
+  }
+  if ((sensorInput[2]<100));
+  {
+    Serial.print("Open Left");
+    Serial.print(" ");
+    microM.Motors(0,forwardSpeedR,0,0);
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(forwardSpeedL,forwardSpeedR,0,0);   
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(forwardSpeedL,0,0,0);
+    delay(50);
+    microM.Motors(0,0,0,0);
+    delay(50);
+    microM.Motors(forwardSpeedL,forwardSpeedR,0,0);
+  }
+}
+
 }
 
 void loop()
 {
+ if(microM.ircommand>0)
+  {
+    Serial.print("\tIR command:");
+    Serial.println(microM.ircommand,DEC);
+  }
   for (int i = 0; i < numSensors; i++)
-{  
-  if (i == 0)
   {
-    sensorInput[i]=analogRead(sensePin[i])-350;
-    Serial.print(sensorInput[i]);
-    Serial.print(" ");
-  } 
-
-  
-  if (i == 1)
-  {
-    sensorInput[i]=analogRead(sensePin[i])-270;
-    Serial.print(sensorInput[i]);
-    Serial.print(" ");
-  } 
-  
-  if (i == 2)
-    {
-    sensorInput[i]=analogRead(sensePin[i])-360;
-    Serial.print(sensorInput[i]);
-    Serial.print(" ");
-  } 
+    sensorData(i); // filling array sensor
  
+    if (sensorInput[1]<220)
+    {
+      microM.Motors(forwardSpeedL,forwardSpeedR,0,0);
+      delay(50);
+      microM.Motors(0,0,0,0);
+      delay(50);
+    }
+
+ remoteControl(microM.ircommand);
+   detectObject(i);
+  }
+  Serial.println();
 }
-Serial.println();
-}
-/*To normalize sensor readings so reading is about 0 
-when nothing is in front of it, write a function subtracting
-value of reading when nothing's in front.*/
+/*Sensor readings are affected by light. Remember to normalize
+in each environment.
+In the loop, i=0 is the RIGHT sensor. i=2 is the LEFT sensor.
+Maybe I could use while loops to have robot avoid obstacles?
+Write*/
